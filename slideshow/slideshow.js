72738569557f4000fe1b48a7d7d8c4b0fb1fc7ab
Blake Richmeier <bdrichmeier@ku.edu>
2021-11-09 19:49:55 -0600

    slide show

--- slideshow/slideshow.js
+++ slideshow/slideshow.js
@@ -1,10 +1,31 @@
-<html>
-  <head>
-      <title> Password Validator </title>
-      <script type="text/javascript" src="webpage.js"> </script>
-</head>
-<body>
-    <input type="password" id="Password1">
-    <input type="password" id="Password2">
-    <button onclick="validatePassword()">Validate</button>
-</body>
+let pictureArr = ["chiefs_logo.png", "jayhawk_logo.png", "royals_logo.png", "skc_logo.png", "chelsea_logo.png"]
+
+index = 0;
+
+function moveRight () {
+	if(index < 4){
+		index = index + 1
+		document.getElementById("1").src = ''
+		document.getElementById("1").src = "pictures/" + pictureArr[index]
+	}
+	else if(index == 4){
+		index = 0;
+		document.getElementById("1").src = ''
+		document.getElementById("1").src = "pictures/" + pictureArr[index]
+	}
+}
+
+function moveLeft () {
+	if(index > 0){
+		index = index - 1
+		document.getElementById("1").src = ''
+		document.getElementById("1").src = "pictures/" + pictureArr[index]
+	}
+	else if(index == 0){
+		index = 4;
+		document.getElementById("1").src = ''
+		document.getElementById("1").src = "pictures/" + pictureArr[index]
+	}
+
+}
+
